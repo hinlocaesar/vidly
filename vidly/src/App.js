656110cec05629690897af38b180986ced12dbd2
Caesar <caesar.hinlo.tech@gmail.com>
2021-10-28 15:45:15 +0800

    Implemented Get dataand display using bootstrap

---
+++ vidly/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Movies from "./component/Movies";
+
+function App() {
+  return (
+    <main className="container">
+      <Movies />
+    </main>
+  );
+}
+
+export default App;
